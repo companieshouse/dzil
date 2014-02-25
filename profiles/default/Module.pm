1b01046e15d465723ba79e3d8485f5b587accf3c
Andrew Maddaford <ajmaddaford@gmail.com>
2014-02-25 16:33:20 +0000

    Pad out Module.pm with some extra stuff

--- profiles/default/Module.pm
+++ profiles/default/Module.pm
@@ -2,6 +2,26 @@ use strict;
 use warnings;
 package {{$name}};
 
-# ABSTRACT: What the module does.
+our $VERSION = '0.01';
+
+sub foo {
+
+}
+
+=encoding utf8
+
+=head1 NAME
+
+{{$name}} - Description of module
+
+=head1 METHODS
+
+L<{{$name}}> implements the following methods.
+
+=head2 foo
+
+Method foo does..
+
+=cut
  
 1;
